1d20da2012bc45cac56e2f91503a8998005a377a
tejasM17 <tejasm9380@gmail.com>
2026-02-12 19:47:39 +0530

    refactor: enhance CORS middleware to support multiple allowed origins - not reset this

--- server.js
+++ server.js
@@ -6,9 +6,17 @@ const chatRoutes = require("./routes/chat.routes");
 
 const app = express();
 
+const allowedOrigins = ["http://localhost:5173", process.env.FRONTEND_URL];
+
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   }),
 );
